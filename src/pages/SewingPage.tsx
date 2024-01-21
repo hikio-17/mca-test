5a9abf0964f82c64570266ba79b7119322bc9351
hikio-17 <fajri010217@gmail.com>
2024-01-21 18:16:13 +0700

    create element search bar

--- src/pages/SewingPage.tsx
+++ src/pages/SewingPage.tsx
@@ -1,9 +1,19 @@
+import { FaSearch } from "react-icons/fa"
 import TableDetailTransaction from "../components/TableDetailTransaction"
 import TableSummary from "../components/TableSummary"
 
 const SewingPage = () => {
   return (
     <section className="sewing-page">
+      <div className="sewing-page__search">
+        <p>
+          Search
+          <span className="sewing-page__search-input">
+            <input type="text" placeholder="🔍" />
+          </span>
+        </p>
+      </div>
+      <hr />
       <div className="sewing-page__summary">
         <h4 className="sewing-page__summary-title">Summary</h4>
 
@@ -12,7 +22,7 @@ const SewingPage = () => {
 
       <div className="sewing-page__detail-transaction">
         <h4 className="sewing-page__detail-transaction-style">SUMMARY</h4>
-        <h4 className="sewing-page__detail-transaction-title">DetailTransaction</h4>
+        <h4 className="sewing-page__detail-transaction-title">Detail Transaction</h4>
         <TableDetailTransaction />
       </div>
     </section>
